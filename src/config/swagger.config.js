fa84e59a6fe3ab1800a0a765d1e1331dca32f7dc
Nicolás Stegmann <nicolas.stegmann@winiksoft.com>
2023-09-26 20:54:24 -0300

    Swagger jwt cookie disclaimer

--- src/config/swagger.config.js
+++ src/config/swagger.config.js
@@ -11,7 +11,11 @@ export default function configureSwagger(app) {
             info: {
                 title: 'Flowery 4107 API',
                 version: '1.0.0',
-                description: 'Flowery 4107 API - Authorization through JWT cookie is not supported in Swagger due to browser security restrictions.',
+                description: `Flowery 4107 API - Authorization through JWT cookie is not supported in Swagger due to browser security restrictions.
+                [More information](https://swagger.io/docs/specification/authentication/cookie-authentication/).
+                \`Alternatively, you can get your cookie jwt credentials using the Flowery 4107 login page:\`
+                [Flowery login page](http://localhost:${process.env.PORT}/login)
+                `,
             },
             servers: [
                 {
